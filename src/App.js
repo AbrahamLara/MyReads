37f21c49868853c403f9b1188a26cc7ab605899c
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-12-27 16:43:41 -0500

    feat: Bookshelves display books retreived using BookAPI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import BookShelf from './BookShelf';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+	state = {
+		currentlyReading: [],
+		wantToRead: [],
+		read: [],
+	}
+
+	componentDidMount() {
+		BooksAPI.getAll().then((books) => {
+			this.setState((currState) => ({
+				currentlyReading: currState.currentlyReading.concat(books.filter((book) => book.shelf === 'currentlyReading')),
+				wantToRead: currState.currentlyReading.concat(books.filter((book) => book.shelf === 'wantToRead')),
+				read: currState.currentlyReading.concat(books.filter((book) => book.shelf === 'read')),
+			}));
+		});
+	}
+
 	render() {
+		const { currentlyReading, wantToRead, read } = this.state;
+
 		return (
-			<div>
+			<div>				
 				<nav className='myreads-title'>MyReads</nav>
-				<BookShelf name='Currently Reading'/>
-				<BookShelf name='Want To Read'/>
-				<BookShelf name='Read'/>
+				<BookShelf id='currentlyReading' books={currentlyReading} name='Currently Reading'/>
+				<BookShelf id='wantToRead' books={wantToRead}  name='Want To Read'/>
+				<BookShelf id='read' books={read}  name='Read'/>
+				<div className='myreads-button'>
+					<button>Search</button>
+				</div>
 			</div>
 		);
 	}
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,12 +3,18 @@ import './BookShelf.css';
 
 class BookShelf extends Component {
     render() {
+        const books = this.props.books;
         return (
             <div className='myreads-bookshelf'>
                 <header className='myreads-bookshelf-header'><strong>{this.props.name}</strong></header>
-                <div className='myreads-bookshelf-books'>
-                    HELLO
-                </div>
+                <ul className='myreads-bookshelf-books'>
+                    {books.map((book) => (
+                        <li key={book.id} className='myreads-bookshelf-book'>
+                            <img alt='Book Thumbnail' src={`${book.imageLinks.thumbnail}`} />
+                            <button>Dropwdown</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
