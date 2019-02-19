dff5862f35b9ba7b6ab09f14cd462bd5a915411d
Abraham Lara Granados <abrahamlara19@gmail.com>
2019-02-19 14:15:51 -0500

    style: Modified book covers to give search page a cleaner look

--- src/Book.js
+++ src/Book.js
@@ -4,31 +4,31 @@ import './css/Book.css';
 const default_cover = 'https://books.google.com/books/content?id=NLK2AAAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
 
 const Book = ({ book, onMovedToNewShelf }) => {
-    const exists = key => key in book;
-    const { imageLinks, title } = book;
-    // Results in Search page that don't have a cover, shelf, or authors are given default values
-    const cover = exists('imageLinks') ? imageLinks.thumbnail : default_cover;
-    const shelf = exists('shelf') ? book.shelf : 'none';
-    const authors = exists('authors') ? book.authors.join(', ') : 'No authors to display';
+	const exists = key => key in book;
+	const { imageLinks, title } = book;
+	// Results in Search page that don't have a cover, shelf, or authors are given default values
+	const cover = exists('imageLinks') ? imageLinks.thumbnail : default_cover;
+	const shelf = exists('shelf') ? book.shelf : 'none';
+	const authors = exists('authors') ? book.authors.join(', ') : 'No authors to display';
 
-    return (
-        <div className='Book'>
-            <div className='book-top'>
-                <img alt='Book Thumbnail' src={cover} />
-                <div className='myreads-bookshelf-book-dropdown'>
-                    <select defaultValue={shelf} onChange={(e) => onMovedToNewShelf(book, e.target.value)}>
-                        <option value='move' disabled>Move to...</option>
-                        <option value='currentlyReading'>Currently Reading</option>
-                        <option value='wantToRead'>Want To Read</option>
-                        <option value='read'>Read</option>
-                        <option value='none'>none</option>
-                    </select>
-                </div>
-            </div>
-            <div className='book-title'>{title}</div>
-            <div className='book-author'>{authors}</div>
-        </div>
-    );
+	return (
+		<div className='Book'>
+			<div className='book-top'>
+				<img className='book-cover' alt='Book Thumbnail' src={cover} />
+				<div className='myreads-bookshelf-book-dropdown'>
+					<select defaultValue={shelf} onChange={(e) => onMovedToNewShelf(book, e.target.value)}>
+						<option value='move' disabled>Move to...</option>
+						<option value='currentlyReading'>Currently Reading</option>
+						<option value='wantToRead'>Want To Read</option>
+						<option value='read'>Read</option>
+						<option value='none'>none</option>
+					</select>
+				</div>
+			</div>
+			<div className='book-title'>{title}</div>
+			<div className='book-author'>{authors}</div>
+		</div>
+	);
 }
 
 export default Book;
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,22 +3,22 @@ import './css/BookShelf.css';
 import Book from './Book';
 
 const BookShelf = ({ books, onBookMoved, name }) => {
-    return (
-        <div className='myreads-bookshelf'>
-            <header className='myreads-bookshelf-header'><strong>{name}</strong></header>
-            <ul className='myreads-bookshelf-books'>
-                {books.map((book) => (
-                    <li key={book.id} className='myreads-bookshelf-book'>
-                        <Book
-                            key={book.id}
-                            book={book}
-                            onMovedToNewShelf={onBookMoved}
-                        />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+	return (
+		<div className='myreads-bookshelf'>
+			<header className='myreads-bookshelf-header'><strong>{name}</strong></header>
+			<ul className='myreads-bookshelf-books'>
+				{books.map((book) => (
+					<li key={book.id} className='myreads-bookshelf-book'>
+						<Book
+							key={book.id}
+							book={book}
+						onMovedToNewShelf={onBookMoved}
+						/>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default BookShelf;
